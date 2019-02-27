5b6ba6e1277774a2a501aab2b78e7694698b6e93
Arion Roberto Krause <arionkrause@trt9.jus.br>
2019-02-27 10:09:52 -0300

    Day 2 Part 2

--- src/day_2.rs
+++ src/day_2.rs
@@ -14,6 +14,7 @@ enum Direction {
 pub fn solve(input: &str) {
     println!("Day {}.", file!().chars().filter(|c| c.is_digit(10)).collect::<String>());
     println!("Part 1: {}.", part_1::solve(&input));
+    println!("Part 2: {}.", part_2::solve(&input));
     println!();
 }
 
@@ -93,5 +94,90 @@ LURDL
 UUUUD";
 
         assert_eq!(solve(input), "1985");
+    }
+}
+
+mod part_2 {
+    use crate::day_2::decode_input;
+    use crate::day_2::Direction;
+
+    pub fn solve(input: &str) -> String {
+        let instructions = decode_input(&input);
+        let mut code = String::new();
+        let mut position = '5';
+
+        for instruction in &instructions {
+            for a_move in &instruction.moves {
+                match a_move {
+                    Direction::Up => {
+                        match position {
+                            '3' => position = '1',
+                            '6' => position = '2',
+                            '7' => position = '3',
+                            '8' => position = '4',
+                            'A' => position = '6',
+                            'B' => position = '7',
+                            'C' => position = '8',
+                            'D' => position = 'B',
+                            _ => {}
+                        }
+                    }
+                    Direction::Right => {
+                        match position {
+                            '2' => position = '3',
+                            '3' => position = '4',
+                            '5' => position = '6',
+                            '6' => position = '7',
+                            '7' => position = '8',
+                            '8' => position = '9',
+                            'A' => position = 'B',
+                            'B' => position = 'C',
+                            _ => {},
+                        }
+                    }
+                    Direction::Down => {
+                        match position {
+                            '1' => position = '3',
+                            '2' => position = '6',
+                            '3' => position = '7',
+                            '4' => position = '8',
+                            '6' => position = 'A',
+                            '7' => position = 'B',
+                            '8' => position = 'C',
+                            'B' => position = 'D',
+                            _ => {},
+                        }
+                    }
+                    Direction::Left => {
+                        match position {
+                            '3' => position = '2',
+                            '4' => position = '3',
+                            '6' => position = '5',
+                            '7' => position = '6',
+                            '8' => position = '7',
+                            '9' => position = '8',
+                            'B' => position = 'A',
+                            'C' => position = 'B',
+                            _ => {},
+                        }
+                    }
+                }
+            }
+
+            code.push(position);
+        }
+
+        code
+    }
+
+    #[cfg(test)]
+    #[test]
+    fn test_1() {
+        let input = "ULL
+RRDDD
+LURDL
+UUUUD";
+
+        assert_eq!(solve(input), "5DB3");
     }
 }
