359e9dc735558ace45a3bebddb806d8c482c2e9c
Arion Roberto Krause <arionkrause@trt9.jus.br>
2019-02-27 15:33:12 -0300

    Day 5 Part 2

--- src/day_5.rs
+++ src/day_5.rs
@@ -1,6 +1,7 @@
 pub fn solve(input: &str) {
     println!("Day {}.", file!().chars().filter(|c| c.is_digit(10)).collect::<String>());
     println!("Part 1: {}.", part_1::solve(&input));
+    println!("Part 2: {}.", part_2::solve(&input));
     println!();
 }
 
@@ -34,5 +35,48 @@ mod part_1 {
     #[test]
     fn test_1() {
         assert_eq!(solve("abc"), "18f47a30");
+    }
+}
+
+mod part_2 {
+    use crypto::digest::Digest;
+    use crypto::md5::Md5;
+
+    pub fn solve(input: &str) -> String {
+        let mut password = String::from("________");
+        let mut index = 0;
+        let mut md5_hasher = Md5::new();
+
+        loop {
+            md5_hasher.input_str(&format!("{}{}", input, index));
+            let result = md5_hasher.result_str();
+
+            if result.starts_with("00000") {
+                let position_as_character = result.chars().nth(5).unwrap();
+
+                match position_as_character.to_digit(10) {
+                    Some(position) => {
+                        if position < 8 && password.chars().nth(position as usize).unwrap() == '_' {
+                            let character = result.chars().nth(6).unwrap();
+                            password.replace_range(position as usize..position as usize + 1, &character.to_string());
+
+                            if !password.contains('_') {
+                                return password;
+                            }
+                        }
+                    }
+                    None => {}
+                }
+            }
+
+            md5_hasher.reset();
+            index += 1;
+        }
+    }
+
+    #[cfg(test)]
+    #[test]
+    fn test_1() {
+        assert_eq!(solve("abc"), "05ace8e3");
     }
 }
