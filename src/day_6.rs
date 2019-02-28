ed415422854d66d38a78aba0ae3d734e8780d139
Arion Roberto Krause <arionkrause@trt9.jus.br>
2019-02-28 09:03:50 -0300

    Day 6 Part 2

--- src/day_6.rs
+++ src/day_6.rs
@@ -1,6 +1,9 @@
+use std::collections::HashMap;
+
 pub fn solve(input: &str) {
     println!("Day {}.", file!().chars().filter(|c| c.is_digit(10)).collect::<String>());
     println!("Part 1: {}.", part_1::solve(&input));
+    println!("Part 2: {}.", part_2::solve(&input));
     println!();
 }
 
@@ -10,22 +13,26 @@ struct LetterFrequency {
     frequency: u8,
 }
 
+fn get_letters_frequencies(input: &str) -> Vec<HashMap<char, LetterFrequency>> {
+    let mut letters_frequencies: Vec<HashMap<char, LetterFrequency>> = vec![HashMap::new(); input.lines().next().unwrap().len()];
+
+    for line in input.lines() {
+        for (index, letter) in line.chars().enumerate() {
+            letters_frequencies[index].entry(letter)
+                    .and_modify(|letter_frequency| letter_frequency.frequency += 1)
+                    .or_insert(LetterFrequency { letter, frequency: 1 });
+        }
+    }
+
+    letters_frequencies
+}
+
 mod part_1 {
-    use std::collections::HashMap;
     use crate::day_6::LetterFrequency;
+    use crate::day_6::get_letters_frequencies;
 
     pub fn solve(input: &str) -> String {
-        let mut letters_frequencies: Vec<HashMap<char, LetterFrequency>> = vec![HashMap::new(); input.lines().next().unwrap().len()];
-
-        for line in input.lines() {
-            for (index, letter) in line.chars().enumerate() {
-                letters_frequencies[index].entry(letter)
-                        .and_modify(|letter_frequency| letter_frequency.frequency += 1)
-                        .or_insert(LetterFrequency { letter, frequency: 1 });
-            }
-        }
-
-        letters_frequencies.into_iter()
+        get_letters_frequencies(&input).into_iter()
                 .map(|letter_frequency| {
                     let mut vector_letter_frequency = letter_frequency.into_iter()
                             .map(|(_, letter_frequency)| letter_frequency)
@@ -57,5 +64,45 @@ dvrsen
 enarar";
 
         assert_eq!(solve(&input), "easter");
+    }
+}
+
+mod part_2 {
+    use crate::day_6::LetterFrequency;
+    use crate::day_6::get_letters_frequencies;
+
+    pub fn solve(input: &str) -> String {
+        get_letters_frequencies(&input).into_iter()
+                .map(|letter_frequency| {
+                    let mut vector_letter_frequency = letter_frequency.into_iter()
+                            .map(|(_, letter_frequency)| letter_frequency)
+                            .collect::<Vec<LetterFrequency>>();
+
+                    vector_letter_frequency.sort_by(|a, b| a.frequency.cmp(&b.frequency));
+                    vector_letter_frequency[0].letter
+                }).collect()
+    }
+
+    #[cfg(test)]
+    #[test]
+    fn test_1() {
+        let input = "eedadn
+drvtee
+eandsr
+raavrd
+atevrs
+tsrnev
+sdttsa
+rasrtv
+nssdts
+ntnada
+svetve
+tesnvt
+vntsnd
+vrdear
+dvrsen
+enarar";
+
+        assert_eq!(solve(&input), "advent");
     }
 }
