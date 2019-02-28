4d7a68f7a57fa4ee9d5ce178f171d87e0b6e9880
Arion Roberto Krause <arionkrause@trt9.jus.br>
2019-02-28 15:52:22 -0300

    Day 5 calculating hashes in parallel chunks

--- src/day_5.rs
+++ src/day_5.rs
@@ -1,3 +1,7 @@
+use crypto::digest::Digest;
+use crypto::md5::Md5;
+use rayon::prelude::*;
+
 pub fn solve(input: &str) {
     println!("Day {}.", file!().chars().filter(|c| c.is_digit(10)).collect::<String>());
     println!("Part 1: {}.", part_1::solve(&input));
@@ -5,32 +9,64 @@ pub fn solve(input: &str) {
     println!();
 }
 
-mod part_1 {
-    use crypto::digest::Digest;
-    use crypto::md5::Md5;
+fn get_hashes_starting_with_five_zeros(key: &[u8], initial_index: usize, batch_size: usize) -> Vec<Vec<u8>> {
+    let amount_cpus = num_cpus::get();
+    let mut chunks_indices = Vec::new();
 
-    pub fn solve(input: &str) -> String {
-        let mut password = String::new();
-        let mut index = 0;
+    for i in 0..amount_cpus {
+        if i == amount_cpus - 1 && batch_size % amount_cpus != 0 {
+            // If batch size is not divisible by the amount of CPUs, the last chunk gets all the remaining items
+            chunks_indices.push((initial_index + i * (batch_size / amount_cpus), initial_index + batch_size));
+        } else {
+            chunks_indices.push((initial_index + i * (batch_size / amount_cpus), initial_index + (i + 1) * (batch_size / amount_cpus)));
+        }
+    }
+
+    chunks_indices.par_iter().map(|(chunk_begin_index, chunk_end_index)| {
+        let mut hashes = Vec::new();
         let mut md5_hasher = Md5::new();
-        let key = input.as_bytes();
 
-        loop {
+        for index in *chunk_begin_index..*chunk_end_index {
             md5_hasher.input(key);
             md5_hasher.input(index.to_string().as_bytes());
             let mut hash = [0; 16];
             md5_hasher.result(&mut hash);
 
             if hash[0] as i32 == 0 && hash[1] as i32 == 0 && (hash[2] >> 4) as i32 == 0 {
-                password.push_str(&format!("{:x}", hash[2]));
-
-                if password.len() == 8 {
-                    return password;
-                }
+                hashes.push(hash.to_vec());
             }
 
             md5_hasher.reset();
-            index += 1;
+        }
+
+        hashes
+    }).flatten()
+            .collect()
+}
+
+mod part_1 {
+    use crate::day_5::get_hashes_starting_with_five_zeros;
+
+    pub fn solve(input: &str) -> String {
+        let mut password = String::new();
+        let mut index = 0;
+        let batch_size = 1_000_000;
+        let key = input.as_bytes();
+
+        loop {
+            let hashes = get_hashes_starting_with_five_zeros(&key, index, batch_size);
+
+            for hash in hashes {
+                if hash[0] as i32 == 0 && hash[1] as i32 == 0 && (hash[2] >> 4) as i32 == 0 {
+                    password.push_str(&format!("{:x}", hash[2]));
+
+                    if password.len() == 8 {
+                        return password;
+                    }
+                }
+            }
+
+            index += batch_size;
         }
     }
 
@@ -42,41 +78,38 @@ mod part_1 {
 }
 
 mod part_2 {
-    use crypto::digest::Digest;
-    use crypto::md5::Md5;
+    use crate::day_5::get_hashes_starting_with_five_zeros;
 
     pub fn solve(input: &str) -> String {
         let mut password = String::from("________");
         let mut index = 0;
-        let mut md5_hasher = Md5::new();
+        let batch_size = 1_000_000;
         let key = input.as_bytes();
 
         loop {
-            md5_hasher.input(key);
-            md5_hasher.input(index.to_string().as_bytes());
-            let mut hash = [0; 16];
-            md5_hasher.result(&mut hash);
+            let hashes = get_hashes_starting_with_five_zeros(&key, index, batch_size);
 
-            if hash[0] as i32 == 0 && hash[1] as i32 == 0 && (hash[2] >> 4) as i32 == 0 {
-                let position_as_character = format!("{:x}", hash[2]).chars().next().unwrap();
+            for hash in hashes {
+                if hash[0] as i32 == 0 && hash[1] as i32 == 0 && (hash[2] >> 4) as i32 == 0 {
+                    let position_as_character = format!("{:x}", hash[2]).chars().next().unwrap();
 
-                match position_as_character.to_digit(10) {
-                    Some(position) => {
-                        if position < 8 && password.chars().nth(position as usize).unwrap() == '_' {
-                            let character = format!("{:x}", hash[3] >> 4).chars().next().unwrap();
-                            password.replace_range(position as usize..position as usize + 1, &character.to_string());
+                    match position_as_character.to_digit(10) {
+                        Some(position) => {
+                            if position < 8 && password.chars().nth(position as usize).unwrap() == '_' {
+                                let character = format!("{:x}", hash[3] >> 4).chars().next().unwrap();
+                                password.replace_range(position as usize..position as usize + 1, &character.to_string());
 
-                            if !password.contains('_') {
-                                return password;
+                                if !password.contains('_') {
+                                    return password;
+                                }
                             }
                         }
+                        None => {}
                     }
-                    None => {}
                 }
             }
 
-            md5_hasher.reset();
-            index += 1;
+            index += batch_size;
         }
     }
 
