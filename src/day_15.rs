ab1756dc53b8326e40eee115e8835eef1c735ab6
Arion Roberto Krause <arionkrause@trt9.jus.br>
2019-03-11 14:59:07 -0300

    Day 15 Part 2

--- src/day_15.rs
+++ src/day_15.rs
@@ -3,6 +3,7 @@ use regex::Regex;
 pub fn solve(input: &str) {
     println!("Day {}.", file!().chars().filter(|c| c.is_digit(10)).collect::<String>());
     println!("Part 1: {}.", part_1::solve(&input));
+    println!("Part 2: {}.", part_2::solve(&input));
     println!();
 }
 
@@ -27,20 +28,23 @@ fn decode_input(input: &str) -> Vec<Disc> {
     }).collect()
 }
 
+fn get_first_valid_time(discs: &Vec<Disc>) -> usize {
+    let mut time = 1;
+
+    loop {
+        if !discs.iter().any(|disc| (disc.starting_position + disc.id + time) % disc.amount_positions != 0) {
+            return time;
+        }
+
+        time += 1;
+    }
+}
+
 mod part_1 {
-    use crate::day_15::{decode_input};
+    use crate::day_15::{decode_input, get_first_valid_time};
 
     pub fn solve(input: &str) -> usize {
-        let discs = decode_input(&input);
-        let mut time = 1;
-
-        loop {
-            if !discs.iter().any(|disc| (disc.starting_position + disc.id + time) % disc.amount_positions != 0) {
-                return time;
-            }
-
-            time += 1;
-        }
+        get_first_valid_time(&decode_input(&input))
     }
 
     #[cfg(test)]
@@ -50,5 +54,22 @@ mod part_1 {
 Disc #2 has 2 positions; at time=0, it is at position 1.";
 
         assert_eq!(solve(&input), 5);
+    }
+}
+
+mod part_2 {
+    use crate::day_15::{Disc, decode_input, get_first_valid_time};
+
+    pub fn solve(input: &str) -> usize {
+        let mut discs = decode_input(&input);
+
+        let new_disc = Disc {
+            id: discs.len() + 1,
+            amount_positions: 11,
+            starting_position: 0,
+        };
+
+        discs.push(new_disc);
+        get_first_valid_time(&discs)
     }
 }
