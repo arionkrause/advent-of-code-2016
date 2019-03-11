eb2fbe8763f23f513316d50eeb704460ec555610
Arion Roberto Krause <arionkrause@trt9.jus.br>
2019-03-11 14:26:54 -0300

    Day 15 Part 1

---
+++ src/day_15.rs
@@ -0,0 +1,54 @@
+use regex::Regex;
+
+pub fn solve(input: &str) {
+    println!("Day {}.", file!().chars().filter(|c| c.is_digit(10)).collect::<String>());
+    println!("Part 1: {}.", part_1::solve(&input));
+    println!();
+}
+
+#[derive(Debug)]
+struct Disc {
+    id: usize,
+    amount_positions: usize,
+    starting_position: usize,
+}
+
+fn decode_input(input: &str) -> Vec<Disc> {
+    let re = Regex::new(r"^Disc #(?P<id>\d+) has (?P<amount_positions>\d+) positions; at time=0, it is at position (?P<starting_position>\d+)\.$").unwrap();
+
+    input.lines().map(|line| {
+        let captures = re.captures(&line).unwrap();
+
+        Disc {
+            id: captures.name("id").unwrap().as_str().parse().unwrap(),
+            amount_positions: captures.name("amount_positions").unwrap().as_str().parse().unwrap(),
+            starting_position: captures.name("starting_position").unwrap().as_str().parse().unwrap(),
+        }
+    }).collect()
+}
+
+mod part_1 {
+    use crate::day_15::{decode_input};
+
+    pub fn solve(input: &str) -> usize {
+        let discs = decode_input(&input);
+        let mut time = 1;
+
+        loop {
+            if !discs.iter().any(|disc| (disc.starting_position + disc.id + time) % disc.amount_positions != 0) {
+                return time;
+            }
+
+            time += 1;
+        }
+    }
+
+    #[cfg(test)]
+    #[test]
+    fn test_1() {
+        let input = "Disc #1 has 5 positions; at time=0, it is at position 4.
+Disc #2 has 2 positions; at time=0, it is at position 1.";
+
+        assert_eq!(solve(&input), 5);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ mod day_11;
 mod day_12;
 mod day_13;
 mod day_14;
-//mod day_15;
+mod day_15;
 //mod day_16;
 //mod day_17;
 //mod day_18;
@@ -104,10 +104,10 @@ fn main() -> Result<(), Error> {
         day_14::solve(&read_file("day_14")?);
     }
 
-//    if (run_all || &arguments[1] == "day_15") && file_exists("day_15") {
-//        day_15::solve(&read_file("day_15")?);
-//    }
-//
+    if (run_all || &arguments[1] == "day_15") && file_exists("day_15") {
+        day_15::solve(&read_file("day_15")?);
+    }
+
 //    if (run_all || &arguments[1] == "day_16") && file_exists("day_16") {
 //        day_16::solve(&read_file("day_16")?);
 //    }
