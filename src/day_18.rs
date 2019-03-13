d8b679a423b0e37b9306bfb838dcf326ed9d13e1
Arion Roberto Krause <arionkrause@trt9.jus.br>
2019-03-13 08:53:59 -0300

    Day 18 Part 2

--- src/day_18.rs
+++ src/day_18.rs
@@ -1,38 +1,44 @@
 pub fn solve(input: &str) {
     println!("Day {}.", file!().chars().filter(|c| c.is_digit(10)).collect::<String>());
     println!("Part 1: {}.", part_1::solve(&input, 40));
+    println!("Part 2: {}.", part_2::solve(&input, 400_000));
     println!();
 }
 
-mod part_1 {
-    pub fn solve(input: &str, amount_rows: usize) -> usize {
-        let mut grid: Vec<Vec<char>> = Vec::with_capacity(amount_rows);
-        grid.push(input.chars().collect());
-        let amount_tiles_per_row = grid[0].len();
+fn get_amount_safe_tiles(input: &str, amount_rows: usize) -> usize {
+    let amount_tiles_per_row = input.len();
+    let mut tiles: Vec<char> = input.chars().collect();
+    let mut buffer: Vec<char> = vec!['.'; amount_tiles_per_row];
+    let mut amount_safe_tiles = tiles.iter().filter(|&&tile| tile == '.').count();
 
-        for _ in 0..amount_rows - 1 {
-            let previous_row_index = grid.len() - 1;
-            let mut row = Vec::with_capacity(amount_tiles_per_row);
+    for _ in 0..amount_rows - 1 {
+        for index in 0..amount_tiles_per_row {
+            let previous_row_left = if index == 0 || tiles[index - 1] == '.' { '.' } else { '^' };
+            let previous_row_center = if tiles[index] == '.' { '.' } else { '^' };
+            let previous_row_right = if index == amount_tiles_per_row - 1 || tiles[index + 1] == '.' { '.' } else { '^' };
 
-            for index in 0..amount_tiles_per_row {
-                let previous_row_left = if index == 0 || grid[previous_row_index][index - 1] == '.' { '.' } else { '^' };
-                let previous_row_center = if grid[previous_row_index][index] == '.' { '.' } else { '^' };
-                let previous_row_right = if index == amount_tiles_per_row - 1 || grid[previous_row_index][index + 1] == '.' { '.' } else { '^' };
-
-                row.push(if (previous_row_left == '^' && previous_row_center == '^' && previous_row_right == '.')
-                        || (previous_row_left == '.' && previous_row_center == '^' && previous_row_right == '^')
-                        || (previous_row_left == '^' && previous_row_center == '.' && previous_row_right == '.')
-                        || (previous_row_left == '.' && previous_row_center == '.' && previous_row_right == '^') {
-                    '^'
-                } else {
-                    '.'
-                });
-            }
-
-            grid.push(row);
+            buffer[index] = if (previous_row_left == '^' && previous_row_center == '^' && previous_row_right == '.')
+                    || (previous_row_left == '.' && previous_row_center == '^' && previous_row_right == '^')
+                    || (previous_row_left == '^' && previous_row_center == '.' && previous_row_right == '.')
+                    || (previous_row_left == '.' && previous_row_center == '.' && previous_row_right == '^') {
+                '^'
+            } else {
+                '.'
+            };
         }
 
-        grid.iter().flatten().filter(|&&tile| tile == '.').count()
+        amount_safe_tiles += buffer.iter().filter(|&&tile| tile == '.').count();
+        std::mem::swap(&mut tiles, &mut buffer);
+    }
+
+    amount_safe_tiles
+}
+
+mod part_1 {
+    use crate::day_18::get_amount_safe_tiles;
+
+    pub fn solve(input: &str, amount_rows: usize) -> usize {
+        get_amount_safe_tiles(&input, amount_rows)
     }
 
     #[cfg(test)]
@@ -44,5 +50,13 @@ mod part_1 {
     #[test]
     fn test_2() {
         assert_eq!(solve(".^^.^.^^^^", 10), 38);
+    }
+}
+
+mod part_2 {
+    use crate::day_18::get_amount_safe_tiles;
+
+    pub fn solve(input: &str, amount_rows: usize) -> usize {
+        get_amount_safe_tiles(&input, amount_rows)
     }
 }
