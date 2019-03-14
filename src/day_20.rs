1d8d4eaf58485e601b5eaf22def0a1b0352d2374
Arion Roberto Krause <arionkrause@trt9.jus.br>
2019-03-14 11:05:05 -0300

    Day 20 Part 2

--- src/day_20.rs
+++ src/day_20.rs
@@ -1,80 +1,32 @@
 pub fn solve(input: &str) {
+    // Credits: aurele (https://www.reddit.com/r/adventofcode/comments/5jbeqo/2016_day_20_solutions/dbf1awy)
     println!("Day {}.", file!().chars().filter(|c| c.is_digit(10)).collect::<String>());
     println!("Part 1: {}.", part_1::solve(&input));
+    println!("Part 2: {}.", part_2::solve(&input));
     println!();
 }
 
-#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
-struct Range {
-    start: u32,
-    end: u32,
+fn get_sorted_blocked_ranges(input: &str) -> Vec<(u64, u64)> {
+    let mut blocked_ranges: Vec<_> = input.lines().map(|line| {
+        let pair: Vec<_> = line.split('-').map(|value| value.parse::<u64>().unwrap()).collect();
+        (pair[0], pair[1])
+    }).collect();
+
+    blocked_ranges.sort();
+    blocked_ranges
 }
 
 mod part_1 {
-    use crate::day_20::Range;
+    use crate::day_20::get_sorted_blocked_ranges;
 
-    pub fn solve(input: &str) -> u32 {
-        let mut blocked_ranges: Vec<Range> = Vec::new();
-
-        'line: for line in input.lines() {
-            let range: Vec<&str> = line.split('-').collect();
-
-            let new_range = Range {
-                start: range[0].parse().unwrap(),
-                end: range[1].parse().unwrap(),
-            };
-
-            blocked_ranges.push(new_range);
-            join_ranges(&mut blocked_ranges);
-        }
-
-        blocked_ranges.sort();
-
-        if blocked_ranges[0].start == 0 {
-            return blocked_ranges[0].end + 1;
-        }
-
-        0
-    }
-
-    fn join_ranges(ranges: &mut Vec<Range>) {
-        loop {
-            let mut join = None;
-            let mut remove = None;
-
-            'range: for (index, range) in ranges.iter().enumerate() {
-                for (index_other, other_range) in ranges.iter().enumerate() {
-                    if index == index_other {
-                        continue;
-                    }
-
-                    if range.start > 0
-                            && other_range.start < range.start
-                            && other_range.end >= range.start - 1
-                            && other_range.end < range.end {
-                        join = Some((index, index_other));
-                        break 'range;
-                    }
-
-                    if range.start >= other_range.start
-                            && range.end <= other_range.end {
-                        remove = Some(index);
-                    }
-                }
+    pub fn solve(input: &str) -> u64 {
+        get_sorted_blocked_ranges(&input).iter().fold(0, |highest, &(low, high)| {
+            if low > highest {
+                return highest;
             }
 
-            match join {
-                Some((index, index_other)) => ranges[index].start = ranges[index_other].start,
-                None => {
-                    match remove {
-                        Some(index) => {
-                            ranges.remove(index);
-                        }
-                        None => return,
-                    }
-                }
-            }
-        }
+            highest.max(high + 1)
+        })
     }
 
     #[cfg(test)]
@@ -85,5 +37,22 @@ mod part_1 {
 4-7";
 
         assert_eq!(solve(&input), 3);
+    }
+}
+
+mod part_2 {
+    use crate::day_20::get_sorted_blocked_ranges;
+
+    pub fn solve(input: &str) -> u64 {
+        get_sorted_blocked_ranges(&input).iter()
+                .chain(std::iter::once(&(1 << 32, 1 << 32)))
+                .fold((0, 0), |(highest, amount), &(low, high)| {
+                    if low > highest {
+                        (high + 1, amount + low - highest)
+                    } else {
+                        (highest.max(high + 1), amount)
+                    }
+                })
+                .1
     }
 }
