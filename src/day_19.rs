c58a5aa35ae94185ae49473d68e51e29fefe7d17
Arion Roberto Krause <arionkrause@trt9.jus.br>
2019-03-14 09:04:39 -0300

    Day 19 optimization

--- src/day_19.rs
+++ src/day_19.rs
@@ -8,34 +8,8 @@ pub fn solve(input: &str) {
 mod part_1 {
     pub fn solve(input: &str) -> usize {
         let input_as_number = input.parse::<usize>().unwrap();
-        let mut elves_presents: Vec<usize> = vec![1; input_as_number];
-        let mut amount_elves_with_presents = input_as_number;
-        let mut index = 0;
-
-        loop {
-            if elves_presents[index] > 0 {
-                let mut next_playing_elf_index = (index + 1) % elves_presents.len();
-
-                loop {
-                    if elves_presents[next_playing_elf_index] > 0 {
-                        elves_presents[index] += elves_presents[next_playing_elf_index];
-                        elves_presents[next_playing_elf_index] = 0;
-                        amount_elves_with_presents -= 1;
-
-                        if amount_elves_with_presents == 1 {
-                            return index + 1;
-                        }
-
-                        index = (next_playing_elf_index + 1) % elves_presents.len();
-                        break;
-                    }
-
-                    next_playing_elf_index = (next_playing_elf_index + 1) % elves_presents.len();
-                }
-            } else {
-                index = (index + 1) % elves_presents.len();
-            }
-        }
+        let highest_power_of_two_lower_than_input = 2usize.pow((input_as_number as f32).log2() as u32) as usize;
+        1 + 2 * (input_as_number - highest_power_of_two_lower_than_input)
     }
 
     #[cfg(test)]
