51ab42d7f3a83d63c996fc31e1dfde9f46bfbf7e
Arion Roberto Krause <arionkrause@trt9.jus.br>
2019-03-15 11:06:56 -0300

    Day 21 Part 2

--- src/day_21.rs
+++ src/day_21.rs
@@ -1,111 +1,132 @@
+use std::collections::VecDeque;
+use regex::Regex;
+
 pub fn solve(input: &str) {
     println!("Day {}.", file!().chars().filter(|c| c.is_digit(10)).collect::<String>());
     println!("Part 1: {}.", part_1::solve(&input, "abcdefgh"));
+    println!("Part 2: {}.", part_2::solve(&input, "fbgdceah"));
     println!();
 }
 
-mod part_1 {
-    use regex::Regex;
-    use std::collections::VecDeque;
+fn process(input: &str, password: &str, unscramble: bool) -> String {
+    let re_swap_position = Regex::new(r"^swap position (?P<position_a>\d+) with position (?P<position_b>\d+)$").unwrap();
+    let re_swap_letter = Regex::new(r"^swap letter (?P<letter_a>[a-z]) with letter (?P<letter_b>[a-z])$").unwrap();
+    let re_rotate = Regex::new(r"^rotate (?P<direction>left|right) (?P<amount_steps>\d+) steps?$").unwrap();
+    let re_rotate_based_on_position = Regex::new(r"^rotate based on position of letter (?P<letter>[a-z]+)$").unwrap();
+    let re_reverse_positions = Regex::new(r"^reverse positions (?P<position_a>\d+) through (?P<position_b>\d+)$").unwrap();
+    let re_move_position = Regex::new(r"^move position (?P<position_a>\d+) to position (?P<position_b>\d+)$").unwrap();
+    let mut password_vector: VecDeque<char> = password.chars().collect();
 
-    pub fn solve(input: &str, password: &str) -> String {
-        let re_swap_position = Regex::new(r"^swap position (?P<position_a>\d+) with position (?P<position_b>\d+)$").unwrap();
-        let re_swap_letter = Regex::new(r"^swap letter (?P<letter_a>[a-z]) with letter (?P<letter_b>[a-z])$").unwrap();
-        let re_rotate = Regex::new(r"^rotate (?P<direction>left|right) (?P<amount_steps>\d+) steps?$").unwrap();
-        let re_rotate_based_on_position = Regex::new(r"^rotate based on position of letter (?P<letter>[a-z]+)$").unwrap();
-        let re_reverse_positions = Regex::new(r"^reverse positions (?P<position_a>\d+) through (?P<position_b>\d+)$").unwrap();
-        let re_move_position = Regex::new(r"^move position (?P<position_a>\d+) to position (?P<position_b>\d+)$").unwrap();
-        let mut scrambled_password: VecDeque<char> = password.chars().collect();
+    let lines: Vec<&str> = if unscramble {
+        input.lines().rev().collect()
+    } else {
+        input.lines().collect()
+    };
 
-        for line in input.lines() {
-            if let Some(captures) = re_swap_position.captures(&line) {
-                let position_a = captures.name("position_a").unwrap().as_str().parse().unwrap();
-                let position_b = captures.name("position_b").unwrap().as_str().parse().unwrap();
-                scrambled_password.swap(position_a, position_b);
-                continue;
-            }
+    for line in lines {
+        if let Some(captures) = re_swap_position.captures(&line) {
+            let position_a = captures.name("position_a").unwrap().as_str().parse().unwrap();
+            let position_b = captures.name("position_b").unwrap().as_str().parse().unwrap();
+            password_vector.swap(position_a, position_b);
+        } else if let Some(captures) = re_swap_letter.captures(&line) {
+            let letter_a = captures.name("letter_a").unwrap().as_str().chars().next().unwrap();
+            let letter_b = captures.name("letter_b").unwrap().as_str().chars().next().unwrap();
 
-            if let Some(captures) = re_swap_letter.captures(&line) {
-                let letter_a = captures.name("letter_a").unwrap().as_str().chars().next().unwrap();
-                let letter_b = captures.name("letter_b").unwrap().as_str().chars().next().unwrap();
-
-                for index in 0..scrambled_password.len() {
-                    if scrambled_password[index] == letter_a {
-                        scrambled_password[index] = letter_b;
-                    } else if scrambled_password[index] == letter_b {
-                        scrambled_password[index] = letter_a;
-                    }
+            for index in 0..password_vector.len() {
+                if password_vector[index] == letter_a {
+                    password_vector[index] = letter_b;
+                } else if password_vector[index] == letter_b {
+                    password_vector[index] = letter_a;
                 }
-
-                continue;
             }
+        } else if let Some(captures) = re_rotate.captures(&line) {
+            let direction = captures.name("direction").unwrap().as_str();
+            let amount_steps = captures.name("amount_steps").unwrap().as_str().parse().unwrap();
 
-            if let Some(captures) = re_rotate.captures(&line) {
-                let direction = captures.name("direction").unwrap().as_str();
-                let amount_steps = captures.name("amount_steps").unwrap().as_str().parse().unwrap();
-
-                for _ in 0..amount_steps {
-                    match direction {
-                        "left" => rotate_left(&mut scrambled_password),
-                        "right" => rotate_right(&mut scrambled_password),
-                        _ => panic!(),
-                    }
+            for _ in 0..amount_steps {
+                match direction {
+                    "left" => if unscramble {
+                        rotate_right(&mut password_vector)
+                    } else {
+                        rotate_left(&mut password_vector)
+                    },
+                    "right" => if unscramble {
+                        rotate_left(&mut password_vector)
+                    } else {
+                        rotate_right(&mut password_vector)
+                    },
+                    _ => panic!(),
                 }
-
-                continue;
             }
+        } else if let Some(captures) = re_rotate_based_on_position.captures(&line) {
+            let letter = captures.name("letter").unwrap().as_str().chars().next().unwrap();
+            let position = password_vector.iter().position(|&a_letter| a_letter == letter).unwrap();
 
-            if let Some(captures) = re_rotate_based_on_position.captures(&line) {
-                let letter = captures.name("letter").unwrap().as_str().chars().next().unwrap();
-                let mut amount_steps = scrambled_password.iter().position(|&a_letter| a_letter == letter).unwrap();
-
-                if amount_steps >= 4 {
-                    amount_steps += 1;
+            let amount_steps = if unscramble {
+                match position {
+                    0 => 1,
+                    1 => 1,
+                    2 => 6,
+                    3 => 2,
+                    4 => 7,
+                    5 => 3,
+                    6 => 0,
+                    7 => 4,
+                    _ => panic!(),
                 }
-
-                for _ in 0..=amount_steps {
-                    rotate_right(&mut scrambled_password);
+            } else {
+                if position >= 4 {
+                    position + 2
+                } else {
+                    position + 1
                 }
+            };
 
-                continue;
-            }
-
-            if let Some(captures) = re_reverse_positions.captures(&line) {
-                let position_a: usize = captures.name("position_a").unwrap().as_str().parse().unwrap();
-                let position_b: usize = captures.name("position_b").unwrap().as_str().parse().unwrap();
-                let slice: Vec<char> = scrambled_password.drain(position_a..=position_b).collect();
-
-                for character in slice {
-                    scrambled_password.insert(position_a, character);
+            for _ in 0..amount_steps {
+                if unscramble {
+                    rotate_left(&mut password_vector);
+                } else {
+                    rotate_right(&mut password_vector);
                 }
-
-                continue;
             }
+        } else if let Some(captures) = re_reverse_positions.captures(&line) {
+            let position_a: usize = captures.name("position_a").unwrap().as_str().parse().unwrap();
+            let position_b: usize = captures.name("position_b").unwrap().as_str().parse().unwrap();
+            let slice: Vec<char> = password_vector.drain(position_a..=position_b).collect();
 
-            if let Some(captures) = re_move_position.captures(&line) {
-                let position_a: usize = captures.name("position_a").unwrap().as_str().parse().unwrap();
-                let position_b: usize = captures.name("position_b").unwrap().as_str().parse().unwrap();
-                let letter = scrambled_password.remove(position_a).unwrap();
-                scrambled_password.insert(position_b, letter);
-                continue;
+            for character in slice {
+                password_vector.insert(position_a, character);
             }
-
+        } else if let Some(captures) = re_move_position.captures(&line) {
+            let position_a: usize = captures.name("position_a").unwrap().as_str().parse().unwrap();
+            let position_b: usize = captures.name("position_b").unwrap().as_str().parse().unwrap();
+            let letter = password_vector.remove(if unscramble { position_b } else { position_a }).unwrap();
+            password_vector.insert(if unscramble { position_a } else { position_b }, letter);
+        } else {
             panic!();
         }
-
-        scrambled_password.iter().collect()
     }
 
-    fn rotate_left(deque: &mut VecDeque<char>) {
-        if let Some(popped_front) = deque.pop_front() {
-            deque.push_back(popped_front);
-        }
-    }
+    password_vector.iter().collect()
+}
 
-    fn rotate_right(deque: &mut VecDeque<char>) {
-        if let Some(popped_back) = deque.pop_back() {
-            deque.push_front(popped_back);
-        }
+fn rotate_left(deque: &mut VecDeque<char>) {
+    if let Some(popped_front) = deque.pop_front() {
+        deque.push_back(popped_front);
+    }
+}
+
+fn rotate_right(deque: &mut VecDeque<char>) {
+    if let Some(popped_back) = deque.pop_back() {
+        deque.push_front(popped_back);
+    }
+}
+
+mod part_1 {
+    use crate::day_21::process;
+
+    pub fn solve(input: &str, password: &str) -> String {
+        process(&input, password, false)
     }
 
     #[cfg(test)]
@@ -121,5 +142,13 @@ rotate based on position of letter b
 rotate based on position of letter d";
 
         assert_eq!(solve(&input, "abcde"), "decab");
+    }
+}
+
+mod part_2 {
+    use crate::day_21::process;
+
+    pub fn solve(input: &str, password: &str) -> String {
+        process(&input, password, true)
     }
 }
